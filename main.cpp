b471c720710ef4ce563719c4458fa9932c171aca
Matthieu Foucault <foucault.matthieu@gmail.com>
2011-12-09 18:09:40 +0100

    continuing main

--- main.cpp
+++ main.cpp
@@ -19,107 +19,127 @@
 using namespace std;
 
 void usage() {
-    cout << "usage: VertexCover inputFile -a algorithm" << endl;
-    cout << "read the manual for more details" << endl;
+	cout << "usage: VertexCover inputFile -a algorithm" << endl;
+	cout << "read the manual for more details" << endl;
 }
 
 void testComplexMinisat() {
 
-    Minisat ms;
-    //    TreeDynamicSons t(40);
-    //    BipartiteGraph t(80, 0.2, 0.6);
-    SimpleGraph t(100, 0.2);
-    cout << "Edge count : " << t.getEdgeCount() << endl;
-    list<int> vertexCoverComplex;
+	/*Minisat ms;
+	 //    TreeDynamicSons t(40);
+	 //    BipartiteGraph t(80, 0.2, 0.6);
+	 SimpleGraph t(100, 0.2);
+	 cout << "Edge count : " << t.getEdgeCount() << endl;
+	 list<int> vertexCoverComplex;
 
-    vertexCoverComplex = ms.getMinisatCoverFromComplexSAT(t, 40, "instance/satComplex/satComplex.in", "instance/satComplex/vertexCoverFromSatComplex.out");
+	 vertexCoverComplex = ms.getMinisatCoverFromComplexSAT(t, 40,
+	 "instance/satComplex/satComplex.in",
+	 "instance/satComplex/vertexCoverFromSatComplex.out");
 
-    cout << "The complex minisat cover is :";
-    for (list<int>::iterator ii = vertexCoverComplex.begin(); ii != vertexCoverComplex.end(); ++ii) {
-        cout << *ii << " , ";
-    }
-    cout << endl;
+	 cout << "The complex minisat cover is :";
+	 for (list<int>::iterator ii = vertexCoverComplex.begin();
+	 ii != vertexCoverComplex.end(); ++ii) {
+	 cout << *ii << " , ";
+	 }
+	 cout << endl;
 
-    list<int> vertexCoverSimple = ms.getMinisatCoverFromSimpleSAT(t, "instance/satSimple/satSimple.in", "instance/satSimple/vertexCoverFromSatSimple.out");
-    cout << "The simple minisat cover is :";
-    for (list<int>::iterator ii = vertexCoverSimple.begin(); ii != vertexCoverSimple.end(); ++ii) {
-        cout << *ii << " , ";
-    }
-    cout << endl;
+	 set<int> vertexCoverSimple = ms.getMinisatCoverFromSimpleSAT(t,
+	 "instance/satSimple/satSimple.in",
+	 "instance/satSimple/vertexCoverFromSatSimple.out");
+	 cout << "The simple minisat cover is :";
+	 for (list<int>::iterator ii = vertexCoverSimple.begin();
+	 ii != vertexCoverSimple.end(); ++ii) {
+	 cout << *ii << " , ";
+	 }
+	 cout << endl;
 
-    DrawGraph dg;
-    dg.drawGraph(t, vertexCoverSimple, "instance/pictureGraph/graphSimple.png");
-    dg.drawGraph(t, vertexCoverComplex, "instance/pictureGraph/graphComplex.png");
+	 DrawGraph dg;
+	 dg.drawGraph(t, vertexCoverSimple, "instance/pictureGraph/graphSimple.png");
+	 dg.drawGraph(t, vertexCoverComplex,
+	 "instance/pictureGraph/graphComplex.png");*/
 }
 
 int main(int argc, char* argv[]) {
 
-    if (argc == 1) {
-        testComplexMinisat();
-    } else
-        if (argc == 2
-            && (strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "--test") == 0)) {
-        if (testAll())
-            cout << "Everything is working fine" << endl;
+	bool graphGenerated = false;
 
-    } else if (argc >= 4 && (strcmp(argv[1], "-g") == 0)) {
-        int graphType = atoi(argv[2]);
-        int graphSize = atoi(argv[3]);
-        Graph* graph;
-        switch (graphType) {
+	if (argc == 1) {
+		testComplexMinisat();
+	} else if (argc == 2
+			&& (strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "--test") == 0)) {
+		if (testAll())
+			cout << "Everything is working fine" << endl;
 
-            case 0:
-            { //SimpleGraph
-                float edgeProba = atof(argv[4]);
-                graph = new SimpleGraph(graphSize, edgeProba);
+	} else {
+		for (int i = 1; i < argc; ++i)
 
-                break;
-            }
-            case 1:
-            { //SmallCoverGraph
-                float edgeProba = atof(argv[4]);
-                int coverSize = atoi(argv[5]);
-                graph = new SmallCoverGraph(graphSize, edgeProba, coverSize);
+			if (strcmp(argv[i], "-g") == 0
+					|| strcmp(argv[i], "--generate-graph") == 0) {
 
-                break;
-            }
-            case 2:
-            { //BipartiteGraph
-                float edgeProba = atof(argv[4]);
-                float partRatio = atof(argv[5]);
-                graph = new BipartiteGraph(graphSize, edgeProba, partRatio);
+				if (argc < i + 3) {
+					usage();
+					return EXIT_FAILURE;
+				}
 
-                break;
-            }
-            case 3:
-            { //TreeDynamic
+				int graphType = atoi(argv[i + 1]);
+				int graphSize = atoi(argv[i + 2]);
+				Graph* graph;
+				switch (graphType) {
 
-                graph = new TreeDynamicSons(graphSize);
-                break;
+				case 0: { //SimpleGraph
+					float edgeProba = atof(argv[i + 3]);
+					graph = new SimpleGraph(graphSize, edgeProba);
 
-            }
-            case 4:
-            { //TreeStatic
+					break;
+				}
+				case 1: { //SmallCoverGraph
+					float edgeProba = atof(argv[i + 3]);
+					int coverSize = atoi(argv[i + 4]);
+					graph = new SmallCoverGraph(graphSize, edgeProba,
+							coverSize);
 
-                int sonsCount = atoi(argv[4]);
-                graph = new TreeStaticSons(graphSize, sonsCount);
-                break;
-            }
-            default:
-            {
-                usage();
-                return EXIT_FAILURE;
-            }
+					break;
+				}
+				case 2: { //BipartiteGraph
+					float edgeProba = atof(argv[i + 3]);
+					float partRatio = atof(argv[i + 4]);
+					graph = new BipartiteGraph(graphSize, edgeProba, partRatio);
 
-        }
+					break;
+				}
+				case 3: { //TreeDynamic
 
-        cout << *graph << endl;
-        delete (graph);
+					graph = new TreeDynamicSons(graphSize);
+					break;
 
-    } else {
-        usage();
-        return EXIT_FAILURE;
-    }
+				}
+				case 4: { //TreeStatic
 
-    return EXIT_SUCCESS;
+					int sonsCount = atoi(argv[i + 4]);
+					graph = new TreeStaticSons(graphSize, sonsCount);
+					break;
+				}
+				default: {
+					usage();
+					return EXIT_FAILURE;
+				}
+
+				}
+
+				cout << *graph << endl;
+				delete (graph);
+
+			} // endif --generate-graph
+			else if (strcmp(argv[i], "-a") == 0
+					|| strcmp(argv[i], "--algorithm")) {
+
+				if (argc < i + 3) {
+					usage();
+					return EXIT_FAILURE;
+				}
+
+			}
+	}
+
+	return EXIT_SUCCESS;
 }
