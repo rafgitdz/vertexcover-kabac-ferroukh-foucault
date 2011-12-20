91878e95fb07e6f4eceb669189a446af8ef00872
Matthieu Foucault <foucault.matthieu@gmail.com>
2011-12-20 09:38:42 +0100

    ajout table des matieres, scripts

--- scripts/parametric.sh
+++ scripts/parametric.sh
@@ -5,8 +5,8 @@ echo "" > bench/parametricbis_cs
 vertexCount=100
 for coverSize in `seq 5 20`
 do
-	../VertexCover -g 1 $vertexCount 0.4 $coverSize -out smallCover.txt
+	../VertexCover -g 1 $vertexCount 40 $coverSize -out smallCover.txt
 	echo "$coverSize `../VertexCover -in smallCover.txt -a 6 $coverSize | grep "^cover found" | cut -d" " -f4`" >> bench/parametric_cs
 	echo "$coverSize `../VertexCover -in smallCover.txt -a 7 $coverSize | grep "^cover found" | cut -d" " -f4`" >> bench/parametricbis_cs
-	gnuplot gnuplot_scripts/gnuplot_parametric_cs
+	gnuplot gnuplot_scripts/vc_parametric_cSize
 done
